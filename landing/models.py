4c0a6dd6a003489cd9fbc083e8dd3c3ee5b5b03a
threough <michael.r.smith@siu.edu>
2018-03-16 15:28:34 -0500

    XML=>JSON, adding tags

--- landing/models.py
+++ landing/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.urls import reverse
 #from .validators import validate_file_extension
 from django.contrib.auth.models import User
+from jsonfield import JSONField
 
 class DataSet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    data = models.BinaryField(blank=True) 
+    data = JSONField() 
+
 
 
 
--- landing/views.py
+++ landing/views.py
@@ -3,14 +3,29 @@ from django.views import generic
 from django.template import RequestContext
 from django.views.generic.edit import CreateView
 #from .models import DataFile
+from landing.JSONDataSet import JSONDataSet
+from landing.JSONDataSet import FileType
+
+from pprint import pprint
 
 def index(request):
+    d = JSONDataSet("/home/michael/Desktop/csv.csv", FileType.CSV)
+    pprint(d.json_dict)
+
+    d.AddTag("TAG1")
+    pprint(d.json_dict)
+    d.AddTag("TAG2")
+    pprint(d.json_dict)
+
+    d.SaveDataset(request.user)
+
     return render(request,'index.html')
 
 
 class ImportData(CreateView):
 #	model = DataFile
 	fields = ['title', 'description', 'file']
+        
 	 #template_name = 'import.html'
 	#return render(request,'import.html')
 
