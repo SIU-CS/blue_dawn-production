349cc2628ff64468d95a510be2fd346e83336bd8
threough <michael.r.smith@siu.edu>
2018-03-15 23:16:35 -0500

    XMLDataSet class

--- landing/models.py
+++ landing/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.urls import reverse
-from .validators import validate_file_extension
+#from .validators import validate_file_extension
 from django.contrib.auth.models import User
 
 class DataSet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=25)
-    description = models.CharField(max_length=30)
     data = models.BinaryField(blank=True) 
 
 
