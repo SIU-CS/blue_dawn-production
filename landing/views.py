9d50fe2cd8f90c08c67bd6fa764b91c341769585
threough <michael.r.smith@siu.edu>
2018-03-16 15:37:25 -0500

    removing testing code that shouldn't be there

--- landing/views.py
+++ landing/views.py
@@ -9,16 +9,6 @@ from landing.JSONDataSet import FileType
 from pprint import pprint
 
 def index(request):
-    d = JSONDataSet("/home/michael/Desktop/csv.csv", FileType.CSV)
-    pprint(d.json_dict)
-
-    d.AddTag("TAG1")
-    pprint(d.json_dict)
-    d.AddTag("TAG2")
-    pprint(d.json_dict)
-
-    d.SaveDataset(request.user)
-
     return render(request,'index.html')
 
 class ImportData(CreateView):
