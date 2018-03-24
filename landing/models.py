49d919a7634a416e1089cddc6affbe546ff98b88
Ahmed <ahmad11nf@hotmail.com>
2018-03-24 14:14:46 -0500

    added model class to confirm user's email and update user profile with the confirmed email

--- landing/models.py
+++ landing/models.py
@@ -3,8 +3,19 @@ from django.urls import reverse
 #from .validators import validate_file_extension
 from django.contrib.auth.models import User
 from jsonfield import JSONField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class DataSet(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	data = JSONField() 
 
+class Confirm(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	confirm = models.BooleanField(default = False)
+
+@receiver(post_save, sender=User)
+class update_profile(sender, instance, created, **kwargs):
+	if created:
+		Confirm.objects.create(user=instance)
+	instance.confirm.save()
