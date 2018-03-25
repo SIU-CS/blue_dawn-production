adb021725d0bf21df67a9ebb7a2da9e4a982da08
Ahmed <ahmad11nf@hotmail.com>
2018-03-24 22:29:57 -0500

    added urls for reset password

--- website/urls.py
+++ website/urls.py
@@ -20,6 +20,8 @@ from landing import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
@@ -33,6 +35,11 @@ urlpatterns = [
 	url(r'^signup$', views.signup, name ="signup"),
 	url(r'^email_activation_sent/$', views.email_activation_sent, name='email_activation_sent'),
 	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+	url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
