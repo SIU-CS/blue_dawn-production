0b8b38f3a66988e7f366248c43b4c9dd78a26c51
Ahmed <ahmad11nf@hotmail.com>
2018-04-09 17:46:00 -0500

    added

--- website/settings.py
+++ website/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'Ahmedalmutairi'
-EMAIL_HOST_PASSWORD = 'Ahmed@123'
+EMAIL_HOST_PASSWORD = 'Hanin@123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
