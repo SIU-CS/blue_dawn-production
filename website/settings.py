071bbe917e981d92a0c12cd229f1b67f53122521
threough <michael.r.smith@siu.edu>
2018-04-13 23:34:16 -0500

    ..with the fix

--- website/settings.py
+++ website/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'BlueDawnAnalysis',
 		'USER': 'root',
-		'PASSWORD': 'Still @live!',
+		'PASSWORD': '',
 		'HOST': '',
 		'PORT': '',
 	}
