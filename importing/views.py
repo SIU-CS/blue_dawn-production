84b70e883ea52d18571a6eb548838f4255b99b5b
threough <michael.r.smith@siu.edu>
2018-04-13 23:28:18 -0500

    cleaning up imports

--- importing/views.py
+++ importing/views.py
@@ -1,34 +1,10 @@
 from django.shortcuts import render
-from templates import imported_libs
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.views import generic
-from django.template import RequestContext
-from django.views.generic.edit import CreateView, UpdateView
 from templates.form import FileForm
-from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect, HttpResponse
-from templates.JSONDataSet import JSONDataSet
-from templates.JSONDataSet import FileType
-from templates.JSONDataSet import InputException
 from django.core.files.storage import default_storage
 from django.conf import settings
+from templates.JSONDataSet import JSONDataSet, FileType, InputException
 import os
-import json
-from templates.models import DataSet
-from django.contrib.auth import login
-from django.contrib.sites.shortcuts import get_current_site
-from django.shortcuts import render, redirect
-from django.utils.encoding import force_bytes, force_text, smart_str
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from templates.form import RegistrationForm
-from login.tokens import accountActivation
-from django.core.mail import EmailMessage
-from django.core.files import File
-from django.contrib.auth.decorators import login_required
-from pprint import pprint
-from django.http import JsonResponse
 
 @login_required(login_url="/login")
 def importing(request):
--- templates/views.py
+++ templates/views.py
@@ -1,33 +1,9 @@
 from django.shortcuts import render
-from templates import imported_libs
-from django.shortcuts import render
-from django.views import generic
-from django.template import RequestContext
-from django.views.generic.edit import CreateView, UpdateView
-from .form import FileForm
-from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponse
 from templates.JSONDataSet import JSONDataSet
-from templates.JSONDataSet import FileType
-from templates.JSONDataSet import InputException
-from django.core.files.storage import default_storage
-from django.conf import settings
-import os
-import json
-from .models import DataSet
-from django.contrib.auth import login
-from django.contrib.sites.shortcuts import get_current_site
-from django.shortcuts import render, redirect
-from django.utils.encoding import force_bytes, force_text, smart_str
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from .form import RegistrationForm
-from login.tokens import accountActivation
-from django.core.mail import EmailMessage
 from django.core.files import File
-from django.contrib.auth.decorators import login_required
-from pprint import pprint
-from django.http import JsonResponse
+from django.utils.encoding import smart_str
+import os
 
 def ExportCSV(request):
     dataset = JSONDataSet.GetDataset(request.GET.get('id'))
@@ -40,7 +16,7 @@ def ExportCSV(request):
     response['Content-Disposition'] = 'attachment; filename=%s' % smart_str(file_name)
 
     os.remove("media/tmp/" + file_name)
-    
+
     return response
 
 def ExportXLSX(request):
