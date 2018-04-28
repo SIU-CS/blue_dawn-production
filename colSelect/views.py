66a53026d40894701552d8ad237a47f26341920b
threough <michael.r.smith@siu.edu>
2018-04-28 14:19:04 -0500

    populating columns, redirection on import

--- colSelect/views.py
+++ colSelect/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from templates.JSONDataSet import JSONDataSet
 
 def colSelect(request):
-    
-    return render(request,'colSelect.html')
+    parameter = request.GET.get('dataset', '')
+    if parameter != "":
+        dataset = JSONDataSet.GetDataset(parameter)
+    else:
+        return render(request,'index.html')
+
+    questions = dataset.GetQuestions()
+    context = {
+        'questions': questions
+    }
+    return render(request, 'colSelect.html', context)
