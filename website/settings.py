54d2f04a4a1a57d5e23743d58ab973a3cf742b97
njcsmajor <nathan.jean@siu.edu>
2018-05-02 23:00:54 -0500

    minor issue

--- website/settings.py
+++ website/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'BlueDawnAnalysis',
         'USER': 'root',
-        'PASSWORD': 'ABarron505HHS+15',
+        'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
